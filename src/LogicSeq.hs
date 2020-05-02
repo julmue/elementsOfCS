c19a828395fe92366142f44bce0db69c7dfd6918
Julian Mueller <Julian.Mueller@snpgroup.com>
2020-05-02 16:52:27 +0200

    rewrite without signals

--- src/LogicSeq.hs
+++ src/LogicSeq.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module LogicSeq ( 
     dff
   , bit
@@ -24,6 +27,12 @@ type KByte1 = Vector8 Byte128
 type KByte8 = Vector8 KByte1
 type KByte64 = Vector8 KByte8
 
+dff_ :: Bit -> State Bit Bit
+dff_ in_ = do
+  memory <- get
+  put in_
+  return memory
+
 dff :: Signal Bit -> State Bit (Signal Bit)
 dff signal = do
   past <- get 
@@ -31,35 +40,37 @@ dff signal = do
   signal' <- dff (S.tail signal)
   return $ past <:> signal'
 
+bit_ :: Bit -> Bit -> State Bit Bit
+bit_ in_ load = mfix (\out -> dff_ (mux out in_ load))
+
 bit :: Signal Bit -> Signal Bit -> State Bit (Signal Bit)
 bit in_ load = mfix (\out -> dff $ S.zipWith3 mux out in_ load)
+
+register_ :: Bit16 -> Bit -> State Bit16 Bit16
+register_ = undefined 
 
 register :: Signal Bit16 -> Signal Bit -> State Bit16 (Signal Bit16)
 register in_ load = do 
   (s01,s02,s03,s04,s05,s06,s07,s08,s09,s10,s11,s12,s13,s14,s15,s16) <- get
   let (in_01,in_02,in_03,in_04,in_05,in_06,in_07,in_08,in_09,in_10,in_11,in_12,in_13,in_14,in_15,in_16) = unzipSignalBit16 in_
-      (out01, s01') = runBit in_01 load s01
-      (out02, s02') = runBit in_02 load s02 
-      (out03, s03') = runBit in_03 load s03 
-      (out04, s04') = runBit in_04 load s04 
-      (out05, s05') = runBit in_05 load s05 
-      (out06, s06') = runBit in_06 load s06 
-      (out07, s07') = runBit in_07 load s07 
-      (out08, s08') = runBit in_08 load s08 
-      (out09, s09') = runBit in_09 load s09 
-      (out10, s10') = runBit in_10 load s10 
-      (out11, s11') = runBit in_11 load s11 
-      (out12, s12') = runBit in_12 load s12
-      (out13, s13') = runBit in_13 load s13
-      (out14, s14') = runBit in_14 load s14
-      (out15, s15') = runBit in_15 load s15
-      (out16, s16') = runBit in_16 load s16 
+      (out01, s01') = run bit in_01 load s01
+      (out02, s02') = run bit in_02 load s02 
+      (out03, s03') = run bit in_03 load s03 
+      (out04, s04') = run bit in_04 load s04 
+      (out05, s05') = run bit in_05 load s05 
+      (out06, s06') = run bit in_06 load s06 
+      (out07, s07') = run bit in_07 load s07 
+      (out08, s08') = run bit in_08 load s08 
+      (out09, s09') = run bit in_09 load s09 
+      (out10, s10') = run bit in_10 load s10 
+      (out11, s11') = run bit in_11 load s11 
+      (out12, s12') = run bit in_12 load s12
+      (out13, s13') = run bit in_13 load s13
+      (out14, s14') = run bit in_14 load s14
+      (out15, s15') = run bit in_15 load s15
+      (out16, s16') = run bit in_16 load s16 
   put (s01',s02',s03',s04',s05',s06',s07',s08',s09',s10',s11',s12',s13',s14',s15',s16')
   return $ zipSignalBit16 (out01,out02,out03,out04,out05,out06,out07,out08,out09,out10,out11,out12,out13,out14,out15,out16)
-    where 
-      
-runBit :: Signal Bit -> Signal Bit -> Bit -> (Signal Bit, Bit)
-runBit in_ load initial = runState (bit in_ load) initial
 
 -- inputs: 
 --    in[16]
@@ -71,23 +82,34 @@ runBit in_ load initial = runState (bit in_ load) initial
 --    out(t)=RAM[adress(t)](t)
 --    
 
+ramn component dmuxNWay muxNWay16 (in_ :: Stream Bit16) address (load :: Stream Bit) = do
+    (Vector8 s01 s02 s03 s04 s05 s06 s07 s08) <- get
+    let (load01, load02, load03, load04, load05, load06, load07, load08) = unzipSignalBit8 $ S.zipWith dmuxNWay load address
+        (out01, s01') = run component in_ load01 s01
+        (out02, s02') = run component in_ load02 s02
+        (out03, s03') = run component in_ load03 s03
+        (out04, s04') = run component in_ load04 s04
+        (out05, s05') = run component in_ load05 s05
+        (out06, s06') = run component in_ load06 s06
+        (out07, s07') = run component in_ load07 s07
+        (out08, s08') = run component in_ load08 s08
+    put (Vector8 s01' s02' s03' s04' s05' s06' s07' s08')
+    return $ zipWith9 muxNWay16 out01 out02 out03 out04 out05 out06 out07 out08 address 
+
 ram8 :: Signal Bit16 -> Signal Bit3 -> Signal Bit -> State Byte16 (Signal Bit16)
 ram8 in_ address load = do
     (Vector8 s01 s02 s03 s04 s05 s06 s07 s08) <- get
     let (load01, load02, load03, load04, load05, load06, load07, load08) = unzipSignalBit8 $ S.zipWith dmux8Way load address
-        (out01, s01') = runRegister in_ load01 s01
-        (out02, s02') = runRegister in_ load02 s02
-        (out03, s03') = runRegister in_ load03 s03
-        (out04, s04') = runRegister in_ load04 s04
-        (out05, s05') = runRegister in_ load05 s05
-        (out06, s06') = runRegister in_ load06 s06
-        (out07, s07') = runRegister in_ load07 s07
-        (out08, s08') = runRegister in_ load08 s08
+        (out01, s01') = run register in_ load01 s01
+        (out02, s02') = run register in_ load02 s02
+        (out03, s03') = run register in_ load03 s03
+        (out04, s04') = run register in_ load04 s04
+        (out05, s05') = run register in_ load05 s05
+        (out06, s06') = run register in_ load06 s06
+        (out07, s07') = run register in_ load07 s07
+        (out08, s08') = run register in_ load08 s08
     put (Vector8 s01' s02' s03' s04' s05' s06' s07' s08')
     return $ zipWith9 mux8Way16 out01 out02 out03 out04 out05 out06 out07 out08 address 
-
-runRegister :: Signal Bit16 -> Signal Bit -> Bit16 -> (Signal Bit16, Bit16) 
-runRegister in_ load initial = runState (register in_ load) initial
 
 ram64 :: Signal Bit16 -> Signal Bit6 -> Signal Bit -> State Byte128 (Signal Bit16)
 ram64 in_ address load = undefined
@@ -101,7 +123,8 @@ ram4K = undefined
 ram16K :: Signal Bit16 -> Signal Bit14 -> Signal Bit -> State KByte64 (Signal Bit16)
 ram16K = undefined
 
-
+run :: (Signal b -> l -> State s (Signal b)) -> Signal b -> l -> s -> (Signal b, s)
+run component in_ load initial = runState (component in_ load) initial
 
 -- Vector8 Bit16
 data Vector8 a = Vector8 {
@@ -231,5 +254,5 @@ clock = S.interleave i o
 test :: Signal Bit -> [Bit]
 test signal = S.take 10 signal
 
-run :: s -> State s a -> a
-run = flip evalState
+-- run :: s -> State s a -> a
+-- run = flip evalState
